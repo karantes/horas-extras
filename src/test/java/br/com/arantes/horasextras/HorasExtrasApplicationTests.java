ed2047f6df3909f937126b9ff2cbe5690a0b713e
kaiqu <kaiqu@host.docker.internal>
2021-01-05 18:02:40 -0300

    First Commit

---
+++ src/test/java/br/com/arantes/horasextras/HorasExtrasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arantes.horasextras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorasExtrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
